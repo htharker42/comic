26f1d3c44e206ee5742e4620a60a90c46af332a5
htharker42 <htharker@gmail.com>
2018-06-22 16:12:53 -0700

    initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,49 @@
+module ApplicationHelper
+
+
+  	def copyright_generator
+  		HarkerViewTool::Renderer.copywrite 'Peggy Smith', 'All Rights Reserved'
+  	end
+
+    def nav_items
+  		[
+  		{
+  			url: root_path,
+  			title: "Home"
+  		},
+  		{
+  			url: news_path,
+  			title: "News"
+  		},
+  		{
+  			url: about_path,
+  			title: "About Me"
+  		},
+  		{
+  			url: #,
+  			title: "Donate"
+  		},
+  		{
+  			url: archive_path,
+  			title: "Archive"
+  		}
+  		{
+  			url: contact_path,
+  			title: "Contact"
+  		}
+  	]
+  	end
+
+
+  	def nav_helper style, tag_type
+  		nav_links = ''
+
+  		nav_items.each do |item|
+  			nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+  		end
+
+  		nav_links.html_safe
+  	end
+
+
+end
