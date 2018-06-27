3c65650d3b7a8ec3dd6edd9fa3579ac49108f84f
htharker42 <htharker@gmail.com>
2018-06-27 15:43:48 -0700

    added comic data to routes and configured index and archive actions to display data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,14 @@
 class PagesController < ApplicationController
 
   def home
+    @comic = Comic.last
   end
 
   def about
+  end
+
+  def archive
+    @comics = Comic.all.order('created_at ASC')
   end
 
   def news
