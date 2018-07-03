e03ee3c9a170be99b622ad6b5151e0c3683c983b
htharker42 <htharker@gmail.com>
2018-07-03 15:16:50 -0700

    added edit and delete features to archive

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,4 +1,10 @@
 class Comic < ApplicationRecord
+  def comic_order
+    if comic.order == nil
+      comic.order = comic.last.order + 1
+    end
+    comic.order
+  end
 
 #file uploading via CarrierWave
 mount_uploader :thumb_image, ComicUploader
